162d1e48fd220e6531a9060ae297f2cd1d428a65
neurosutton <mohl.annie@gmail.com>
2020-12-01 16:19:16 -0700

    Finish archiving

---
+++ conn_pandas_version/fmri_analysis_bnv_prep.py
@@ -0,0 +1,81 @@
+"""
+Create files for BrainNetViewer (BNV) visualization of network connectivity.
+
+Date: December 2020
+@author: Brianne Sutton, PhD
+v0.1
+"""
+import pandas as pd
+import os
+import fmri_analysis_utilities as utils
+import fmri_analysis_matrices as fam
+import fmri_analysis_load_funcs as faload
+
+config = faload.load_config()
+
+class bnv_analysis():
+    def __init__(self, network=None, label_file=os.path.join(config.atlas_dir,'hcpmmp1_expanded_labels.csv'), group=None, atlas_label="SuttonLabel", subject_list=None, prop_thr=0):
+        self.network = network
+        self.group = group #Filtered group for the node values
+        self.atlas_label = atlas_label
+        self.label_df = pd.DataFrame(pd.read_csv(label_file))
+        self.subject_list = subject_list
+        self.prop_thr = prop_thr
+
+    def clean_labels(self):
+        """Reduce mismatches and extraneous information from label file, so that the bare minimum needed for BNV is merged."""
+        self.label_df = self.label_df[['x','y','z',self.atlas_label]]
+
+    def load_data(self,data_file=None):
+        """Input from functional connectivity matrices that have been limited to the network of interest."""
+        if data_file:
+            try:
+                df = pd.DataFrame(pd.read_csv(data_file))
+            except:
+                try:
+                    df = pd.DataFrame(pd.read_excel(data_file))
+                except Exception as e:
+                    print(e)
+        else:
+            dat = fam.analysis(self.network)
+            self.conn_df = dat.produce_matrix()
+
+    def clean_data(self):
+        """Manipulate df to include data for specific networks and thresholds.Remove unnecessary columns and insure that names will be consistent with merging parameters."""
+        if self.subject_list:
+            self.conn_df = utils.filter_conn_df_subjects(self.conn_df,subject_list)
+        if self.group:
+            self.conn_df = self.conn_df.loc[self.conn_df[config.group_id_col]==self.group]
+        self.conn_df = self.conn_df.loc[self.conn_df['prop_thr']==prop_thr,:]
+        self.check = 'done'
+
+    def make_node_file(self, msr_of_int):
+        if self.check == 'done':      
+            node_df = self.conn_df[[msr_of_int] + ['rois']]
+            #node_df[msr_of_int] = node_df[msr_of_int].mask(~node_df['rois'].str.lower().str.contains(self.network)) # This seems to be a redundant filter for network ROIs. Might be okay, but need to test
+        
+            out_df = label_df.merge(node_df, left_on = self.atlas_label, right_on = 'rois').drop(columns=([self.atlas_label]))
+            out_df['size'] = out_df[msr_of_int]
+            out_df = out_df.apply(lambda x: x.str.strip() if x.dtype=="object" else x)
+            out_df = out_df[['x','y','z',msr_of_int,'size','rois']]
+            out_df.drop_duplicates(inplace=True)
+            out_df.to_csv(op.join(config.conn_dir,str(config.date)+ '_' + self.network + '_' + self.prop_thr + '_bnv.node'),header=False, index=False,sep='\t')
+        else:
+            print(f'Please validate that you are using the correct data by running {self.clean_data}')
+
+    def make_edge_file(self):        
+        if self.check == 'done':
+            edges = self.conn_df
+            edges.drop(columns=([config.name_id_col, config.group_id_col]),inplace=True)
+            edges = edges.replace({np.nan:0})
+            edges_bin = np.where(edges>.1,1,0)
+            np.savetxt(op.join(config.conn_dir,str(config.date)+ '_' + self.network + '_' + self.prop_thr + '_bnv.edge'),edges,delimiter='\t')
+        else:
+            print(f'Please validate that you are using the correct data by running {self.clean_data}')
+
+    def run_bnv_prep(self):
+        self.clean_labels()
+        self.load_data()
+        self.clean_data()
+        self.make_node_file()
+        self.make_edge_file()
---
+++ conn_pandas_version/fmri_analysis_load_funcs.py
@@ -0,0 +1,103 @@
+"""
+Functions to explore connectivity matrices generated through CONN.
+
+Date: August 2020
+@author: Brianne Sutton, PhD from Josh Bear, MD
+v0.3 (BMS) Adapted fmri_analysis_functions for pandas
+"""
+import os
+from glob import glob
+import numpy as np
+import fnmatch, random, time, pickle
+import pandas as pd
+import json
+from collections import OrderedDict, defaultdict
+from datetime import datetime
+from scipy.io import loadmat
+
+
+class config():
+    def __init__(self,__def_path__=None):
+        if __def_path__:
+            self.__def_path__ = __def_path__
+        with open(os.path.join(self.__def_path__,'directory_defs.json')) as f:
+            defs = json.load(f)
+            self.conn_dir = defs['conn_dir']
+            self.main_dir = defs['main_dir']
+            self.atlas_dir = defs['atlas_dir']
+            self.proj_dir = defs['data_dir']
+            global pkl_file
+            pkl_file =  os.path.join(self.proj_dir,'vars.pkl')
+            
+        self.conn_file = 'resultsROI_Condition001.mat'
+        self.nonimaging_subjectlevel_data =  os.path.join(self.main_dir,'eses_subjects_202008.csv')
+
+        self.name_id_col = "BK_name"
+        self.group_id_col = "group"
+        self.msr_dict = {'cc':"clustering coefficienct", 'pl':"path length",'ms':"mean strength", 'mod':"modularity", 'le':"local efficiency"}
+        self.debug = ''
+
+        self.date = datetime.today().strftime('%Y%m')
+        self.mdata,x = load_mat(self.proj_dir, os.path.join(self.conn_dir,self.conn_file))
+        self.prep_pickle()
+     
+    def prep_pickle(self):
+         with open(pkl_file,'wb') as f:
+            pickle.dump(self,f)
+
+def load_config():
+     with open(pkl_file, 'rb') as f:
+        cfg = pickle.load(f)
+     return cfg
+
+def load_mat(proj_dir, conn_file):
+    """Loading and reloading the module is much quicker with loading the matrix as its own method. Call first, so that there is data, though."""
+    mdata = loadmat(os.path.join(proj_dir, conn_file))
+    rois = mdata['names']
+    rois = [roi[0].replace('hcp_atlas.','') for r_array in rois for roi in r_array]
+    return mdata, rois
+
+
+def get_network_parcels(mdata, network_name):
+    """Returns parcel names and indices with HCP remaining in the name and indexed to work with numpy-based functions."""
+    parcel_names = [str[0] for str in mdata['names'][0]]
+    parcels = {k:v for v,k in enumerate([str[0] for str in mdata['names'][0]])}
+    pattern = 'hcp_atlas.' + network_name + '*'
+    matching = fnmatch.filter(parcels.keys(), pattern)
+    network_parcels = {k:v for k,v in parcels.items() if k in matching}
+    indices = [parcels.get(key) for key in matching]
+    return network_parcels
+
+
+def get_parcel_dict(mdata, network_name=None, inverse=False):
+    """Alternate method to get ROI indices and names."""
+    try:
+        parcel_names = [str[0] for str in mdata['names'][0]]
+    except:
+        print(f'Issue in get_parcel_dict(mdata,network_name={network_name}, inverse={inverse})')
+        print(mdata['names'])
+
+    parcel_dict = OrderedDict()
+#    if network_name:
+#        print(f'Selecting ROIs belonging to the {network_name} network.\n')
+    for p,parcel in enumerate(parcel_names):
+        parcel = parcel.replace('hcp_atlas.','') # Clean the names
+        if network_name and ('whole' not in network_name):
+            if network_name.lower() in parcel.lower():
+                parcel_dict[parcel] = p
+            else:
+                pass
+                #print(f'Did not find {network_name.lower()} in {parcel.lower()}')
+        else:
+            parcel_dict[parcel] = p
+    if inverse:
+        parcel_dict = {v:k for k,v in parcel_dict.items()}
+    return parcel_dict
+
+def get_subj_df_data(nonimaging_subjectlevel_data):
+    """Primarily for reading in demographic and neuropsychological data."""
+    cfg = load_config()
+    subj_df = pd.DataFrame(pd.read_csv(nonimaging_subjectlevel_data))
+    subj_dict = {k:v for k,v in enumerate(subj_df[cfg.name_id_col])}
+    group_dict = dict(zip(subj_df[cfg.name_id_col], subj_df[cfg.group_id_col]))
+    return subj_df, subj_dict, group_dict
